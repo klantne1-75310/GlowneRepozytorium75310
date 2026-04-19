e42fc9633c36046ffb063ba913ade4e3580943e3
75310 <klantne1@stu.vistula.edu.pl>
2026-04-19 18:15:08 +0200

    aktualizacja do zadania 5 - 75310

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
+const form = document.getElementById("kontaktForm");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let valid = true;
+
+    let imie = document.getElementById("imie").value.trim();
+    let nazwisko = document.getElementById("nazwisko").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let wiadomosc = document.getElementById("wiadomosc").value.trim();
+
+    // reset błędów
+    document.getElementById("imieError").textContent = "";
+    document.getElementById("nazwiskoError").textContent = "";
+    document.getElementById("emailError").textContent = "";
+    document.getElementById("wiadomoscError").textContent = "";
+
+//  error jak nie ma imienia labo cyrfry
+    if (imie === "") {
+        document.getElementById("imieError").textContent = "Imię jest wymagane";
+        valid = false;
+    } else if (/\d/.test(imie)) {
+        document.getElementById("imieError").textContent = "Imię nie może zawierać cyfr";
+        valid = false;
+    }
+
+//  error jak złe nazwisko
+    if (nazwisko === "") {
+        document.getElementById("nazwiskoError").textContent = "Nazwisko jest wymagane";
+        valid = false;
+    } else if (/\d/.test(nazwisko)) {
+        document.getElementById("nazwiskoError").textContent = "Nazwisko nie może zawierać cyfr";
+        valid = false;
+    }
+
+    //  error jak zły email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (email === "") {
+        document.getElementById("emailError").textContent = "Email jest wymagany";
+        valid = false;
+    } else if (!emailRegex.test(email)) {
+        document.getElementById("emailError").textContent = "Niepoprawny adres email";
+        valid = false;
+    }
+    //  error jak  nie ma wiadomości
+    if (wiadomosc === "") {
+        document.getElementById("wiadomoscError").textContent = "Wiadomość jest wymagana";
+        valid = false;
+    }
+
+    if (valid) {
+        alert("Formularz wysłany poprawnie!");
+        form.reset();
+    }
+});
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const themeButton = document.querySelector('header button:first-of-type');
     const stylesheet = document.querySelector('link[rel="stylesheet"]');
